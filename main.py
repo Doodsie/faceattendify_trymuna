575550b5bdfddd6d26114bbd9df69f8b5bb6842a
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-11-07 19:02:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1280,6 +1280,7 @@ def join():
 ##################################### END USER MANAGEMENT#####################################################
 
 if __name__ == "__main__":
-    app.run()
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
 
