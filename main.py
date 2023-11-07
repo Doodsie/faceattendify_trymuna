2ea6764b98ebcfa2a3dce5bf0a4b6e2074c239ea
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-11-08 04:11:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1282,7 +1282,6 @@ def join():
 ##################################### END USER MANAGEMENT#####################################################
 
 if __name__ == "__main__":
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
-
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
