d8374de018a3f13acc05f6ae7a02b9c51ada174e
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-11-07 20:21:51 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,12 @@
-from gunicorn.app.wsgiapp import run
+from gunicorn.app.base import Application
+from main import app  # Import your Flask app from main.py (or the appropriate file)
+
+class FlaskApp(Application):
+    def init(self, parser, opts, args):
+        pass
+
+    def load(self):
+        return app
+
+if __name__ == "__main__":
+    FlaskApp().run()
