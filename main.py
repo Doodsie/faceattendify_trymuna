bcb0d1ed1396041a900926217e8256753a7f8964
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-11-08 04:27:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1281,7 +1281,10 @@ def join():
 
 ##################################### END USER MANAGEMENT#####################################################
 
+gunicorn -c gunicorn_config.py app:app
+
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run()
+    
+
 
