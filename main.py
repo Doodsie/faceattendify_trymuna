67bf8533e739b5554cacf30f99cf5d4938019f4e
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-11-11 02:37:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ pause_cnt = 0
 justscanned = False
 
 config = {
-    "host": "mysql://root:f4C3ed4bcfAEfachEbC1dfDhBeFdfgA1@mysql.railway.internal:3306/zagusopass",
+    "host": "roundhouse.proxy.rlwy.net",
     "user": "root",
     "password": "f4C3ed4bcfAEfachEbC1dfDhBeFdfgA1",
     "database": "zagusopass"
