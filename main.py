d1003f834f3c72cdb2afd23fa4f82086a3170d26
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-11-12 23:06:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def generate_dataset(nbr):
             cropped_face = img[y:y + h, x:x + w]
         return cropped_face
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
 
     mycursor.execute("select ifnull(max(img_id), 0) from img_dataset")
     row = mycursor.fetchone()
@@ -248,7 +248,7 @@ def face_show():
 
     wCam, hCam = 400, 400
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     cap.set(3, wCam)
     cap.set(4, hCam)
 
