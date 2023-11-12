318ad7fa1d5d285bb115831be318bd2fe4ee043b
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-11-12 22:07:45 +0800

    Try create database for Image dataset

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import time
 from datetime import date, datetime
 import re
 import gunicorn
-
+import base64
 
 app = Flask(__name__)
 app.secret_key = 'pisatindipay'
@@ -29,37 +29,44 @@ config = {
 cnx = mysql.connector.connect(**config)
 mycursor = cnx.cursor(buffered=True)
 
+# Create a new table for image data
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS img_data (
+        img_id INT AUTO_INCREMENT PRIMARY KEY,
+        img_person INT,
+        img_data LONGBLOB,
+        FOREIGN KEY (img_person) REFERENCES your_existing_person_table(person_id)
+    )
+""")
+cnx.commit()
+
+# ...
+
+def encode_image(image):
+    _, buffer = cv2.imencode('.jpg', image)
+    return buffer.tobytes()
+
+
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Generate dataset >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def generate_dataset(nbr):
     face_classifier = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.xml")
 
-    mycursor.execute("select * from img_dataset WHERE img_person='" + str(nbr) + "'")
+    mycursor.execute(f"SELECT * FROM img_dataset WHERE img_person='{nbr}'")
     data1 = mycursor.fetchall()
+    
     for item in data1:
-        imagePath = "dataset/" + nbr + "." + str(item[0]) + ".jpg"
-        # print(imagePath)
+        imagePath = f"dataset/{nbr}.{item[0]}.jpg"
         try:
             os.remove(imagePath)
         except:
             pass
-    mycursor.execute("delete from img_dataset WHERE img_person='" + str(nbr) + "'")
+
+    mycursor.execute(f"DELETE FROM img_dataset WHERE img_person='{nbr}'")
     cnx.commit()
 
-    def face_cropped(img):
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_classifier.detectMultiScale(gray, 1.3, 5)
-        # scaling factor=1.3
-        # Minimum neighbor = 5
+    cap = cv2.VideoCapture(0)
 
-        if len(faces) == 0:
-            return None
-        for (x, y, w, h) in faces:
-            cropped_face = img[y:y + h, x:x + w]
-        return cropped_face
-
-    cap = cv2.VideoCapture(1)
-
-    mycursor.execute("select ifnull(max(img_id), 0) from img_dataset")
+    mycursor.execute("SELECT IFNULL(MAX(img_id), 0) FROM img_dataset")
     row = mycursor.fetchone()
     lastid = row[0]
 
@@ -73,30 +80,32 @@ def generate_dataset(nbr):
             frame1 = cv2.resize(img, (200, 200))
             frame1 = cv2.imencode('.jpg', frame1)[1].tobytes()
             yield (b'--frame1\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame1 + b'\r\n')
+        
         if face_cropped(img) is not None:
             count_img += 1
             img_id += 1
             face = cv2.resize(face_cropped(img), (200, 200))
             face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
-            file_name_path = "dataset/" + nbr + "." + str(img_id) + ".jpg"
-            cv2.imwrite(file_name_path, face)
-            cv2.putText(face, str(count_img) + '%', (5, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
-
-            mycursor.execute("""INSERT INTO `img_dataset` (`img_id`, `img_person`) VALUES
-                                ('{}', '{}')""".format(img_id, nbr))
+            # Store the image data in the new table
+            img_data = encode_image(face)
+            mycursor.execute("""
+                INSERT INTO img_data (img_person, img_data) VALUES (%s, %s)
+            """, (nbr, img_data))
             cnx.commit()
-            if int(img_id) == int(max_imgid):
-                if int(img_id) == int(max_imgid):
-                    cv2.putText(face, "Training Complete", (5, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
-                    cv2.putText(face, "Click Train Face.", (5, 45), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+
+            if img_id == max_imgid:
+                cv2.putText(face, "Training Complete", (5, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                cv2.putText(face, "Click Train Face.", (5, 45), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+
             frame = cv2.imencode('.jpg', face)[1].tobytes()
             yield (b'--frame1\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-            if cv2.waitKey(1) == 13 or int(img_id) == int(max_imgid):
+            if cv2.waitKey(1) == 13 or img_id == max_imgid:
                 break
-                cap.release()
-                cv2.destroyAllWindows()
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Train Classifier >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
