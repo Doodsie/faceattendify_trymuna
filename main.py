9fa1bfdcc577e8b0247660d180623bcd4fc57edb
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-11-14 04:35:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 import time
 from datetime import date, datetime
 import re
-
+asd
 
 
 app = Flask(__name__)
@@ -31,26 +31,9 @@ mycursor = cnx.cursor(buffered=True)
     
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Generate dataset >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def generate_dataset(nbr):
-    face_classifier = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.xml")
-
-    mycursor.execute("select * from img_dataset WHERE img_person='" + str(nbr) + "'")
-    data1 = mycursor.fetchall()
-    for item in data1:
-        imagePath = "dataset/" + nbr + "." + str(item[0]) + ".jpg"
-        # print(imagePath)
-        try:
-            os.remove(imagePath)
-        except:
-            pass
-    mycursor.execute("delete from img_dataset WHERE img_person='" + str(nbr) + "'")
-    cnx.commit()
-
     def face_cropped(img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_classifier.detectMultiScale(gray, 1.3, 5)
-        # scaling factor=1.3
-        # Minimum neighbor = 5
-
         if len(faces) == 0:
             return None
         for (x, y, w, h) in faces:
@@ -73,31 +56,33 @@ def generate_dataset(nbr):
             frame1 = cv2.resize(img, (200, 200))
             frame1 = cv2.imencode('.jpg', frame1)[1].tobytes()
             yield (b'--frame1\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame1 + b'\r\n')
+
         if face_cropped(img) is not None:
             count_img += 1
             img_id += 1
             face = cv2.resize(face_cropped(img), (200, 200))
             face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
-            file_name_path = "dataset/" + nbr + "." + str(img_id) + ".jpg"
+            file_name_path = "dataset/" + str(nbr) + "." + str(img_id) + ".jpg"
             cv2.imwrite(file_name_path, face)
             cv2.putText(face, str(count_img) + '%', (5, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
 
             mycursor.execute("""INSERT INTO `img_dataset` (`img_id`, `img_person`) VALUES
                                 ('{}', '{}')""".format(img_id, nbr))
             cnx.commit()
+
             if int(img_id) == int(max_imgid):
-                if int(img_id) == int(max_imgid):
-                    cv2.putText(face, "Training Complete", (5, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
-                    cv2.putText(face, "Click Train Face.", (5, 45), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                cv2.putText(face, "Training Complete", (5, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                cv2.putText(face, "Click Train Face.", (5, 45), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+
             frame = cv2.imencode('.jpg', face)[1].tobytes()
             yield (b'--frame1\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
             if cv2.waitKey(1) == 13 or int(img_id) == int(max_imgid):
                 break
-                cap.release()
-                cv2.destroyAllWindows()
 
+    cap.release()
+    cv2.destroyAllWindows()
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Train Classifier >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @app.route('/train_classifier/<nbr>')
@@ -421,9 +406,9 @@ def vfdataset_page():
 
 
 @app.route('/vidfeed_dataset/<nbr>')
-def vidfeed_dataset(nbr):
-    # Video streaming route. Put this in the src attribute of an img tag
-    return Response(generate_dataset(nbr), mimetype='multipart/x-mixed-replace; boundary=frame')
+def video_feed(nbr):
+    return Response(gen(nbr),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/video_feed', methods=['GET', 'POST'])
