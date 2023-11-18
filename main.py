d9433c9057c766e2a7842d96267abafa2450434a
Doodsie <84783052+Doodsie@users.noreply.github.com>
2023-11-18 23:27:57 +0800

    gendataset changes

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def generate_dataset(nbr):
     data1 = mycursor.fetchall()
     for item in data1:
         imagePath = "dataset/" + nbr + "." + str(item[0]) + ".jpg"
+        # print(imagePath)
         try:
             os.remove(imagePath)
         except:
@@ -47,6 +48,8 @@ def generate_dataset(nbr):
     def face_cropped(img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+        # scaling factor=1.3
+        # Minimum neighbor = 5
 
         if len(faces) == 0:
             return None
@@ -54,44 +57,46 @@ def generate_dataset(nbr):
             cropped_face = img[y:y + h, x:x + w]
         return cropped_face
 
-    def generate_frames():
-        while True:
-            frame_data = yield
+    cap = cv2.VideoCapture(0)
 
-            nparr = np.frombuffer(frame_data, np.uint8)
-            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    mycursor.execute("select ifnull(max(img_id), 0) from img_dataset")
+    row = mycursor.fetchone()
+    lastid = row[0]
 
-            cropped_face = face_cropped(img)
-
-            if cropped_face is not None:
-                count_img = 0
-                img_id = 0
-                max_imgid = 100
-
-                face = cv2.resize(cropped_face, (200, 200))
-                face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-
-                file_name_path = "dataset/" + nbr + "." + str(img_id) + ".jpg"
-                cv2.imwrite(file_name_path, face)
-                cv2.putText(face, str(count_img) + '%', (5, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
-
-                mycursor.execute("""INSERT INTO `img_dataset` (`img_id`, `img_person`) VALUES
-                                    ('{}', '{}')""".format(img_id, nbr))
-                cnx.commit()
-
-                frame = cv2.imencode('.jpg', face)[1].tobytes()
-                yield (b'--frame1\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-    frame_generator = generate_frames()
-    next(frame_generator)
+    img_id = lastid
+    max_imgid = img_id + 100
+    count_img = 0
 
     while True:
-        try:
-            frame_data = yield
-            frame_generator.send(frame_data)
-        except GeneratorExit:
-            break
+        ret, img = cap.read()
+        if face_cropped(img) is None:
+            frame1 = cv2.resize(img, (200, 200))
+            frame1 = cv2.imencode('.jpg', frame1)[1].tobytes()
+            yield (b'--frame1\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame1 + b'\r\n')
+        if face_cropped(img) is not None:
+            count_img += 1
+            img_id += 1
+            face = cv2.resize(face_cropped(img), (200, 200))
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
+            file_name_path = "dataset/" + nbr + "." + str(img_id) + ".jpg"
+            cv2.imwrite(file_name_path, face)
+            cv2.putText(face, str(count_img) + '%', (5, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+
+            mycursor.execute("""INSERT INTO `img_dataset` (`img_id`, `img_person`) VALUES
+                                ('{}', '{}')""".format(img_id, nbr))
+            cnx.commit()
+            if int(img_id) == int(max_imgid):
+                if int(img_id) == int(max_imgid):
+                    cv2.putText(face, "Training Complete", (5, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(face, "Click Train Face.", (5, 45), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+            frame = cv2.imencode('.jpg', face)[1].tobytes()
+            yield (b'--frame1\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+            if cv2.waitKey(1) == 13 or int(img_id) == int(max_imgid):
+                break
+                cap.release()
+                cv2.destroyAllWindows()
 
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Train Classifier >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
